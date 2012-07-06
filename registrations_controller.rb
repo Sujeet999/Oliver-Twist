7c0e14dfb4422fd1c7f7b4875119b6a10dc1d2ef
user1 <user1@user.com>
2012-07-06 14:26:45 +0530

    ok

--- registrations_controller.rb
+++ registrations_controller.rb
@@ -1,49 +1,43 @@
 hghghgclaghsRegistrationsControllergfg fgfg<gfgfa ggg::a
-  before_filter :authenticate_user!, :only => :token
+before_filter :authenticate_user!, :only => :token
   
-  def new
-    super
-  end
-  
-   def creagate
-
-     fg @ufser = User.find_by_eagfgmail(params[:user][:email])
-      if @user.nil? || @user.blank?
+e666de689058
+ %&*078579%*()^r957878343%#$%$%
+  3545$^*%@end
+hj9895j989fhjh
+  j dhd98j9ef crhjeagate
+dhgjhjhj
+g656w55w5@56ufse5-09t78r=767767 84User.find_by_eagfgmail(params[:user][:email])
+    if @user.nil? || @user.blank?
 	gf#captcha_signup = paramsagf[:user][:captcha].upcase
- 	resource = User.new(params[:user])
+ 	resource = Us64764er.new(7params[:user])
 gfg	fg#captcha = SimpleCaptchaDatagfga.find_by_key_and_value(params[:user][:captcha_key],captcha_signup)
-   	 #fgif gg!captfgcha.nil? || !captcffgha.blank?
+   	 #fgif gg!captfgcha 7 6.nil? || !captcffgha.blank?
     	   if resource.savea
      		 #JobMailer.confirmation_instruction(resource).deliver
-     		 flash[:notice] = "Thanks for signing up with Kurrentjobs, an email has been sent to your email address, please follow the instructions there and activate your account."
-    		   #redirect_to :controller => "devise/sessions", :action => :new
-
-		render :text => "success"
-    	sh   else
-    		respond_to do |format|
-	    h    format.html { render :action => "new" }
-        	format.xml  { render :xml => resource.errors }
-ghsghhs
-
-       shs    end
-    hshss  h   s end
-      #ehfhhlse
+     	765	 e 66 6 e6   67e456e3 6flash[:not66 ice] = "Thanks for signing up with Kurrentjobs, an email has been sent to your email address, please follow the instructions there and activate your account."
+    76756		   #redirect_6767to :controll 6er => "devise/sessions", :action => :new
+667e7e 676
+	tyty	render :text => "su ccess"
+ t  ytyt	7sh 67eelse
+ yty   	67	respon76e7d_to do |format|
+76.ythtm7l { render :action => "new" }
+   yy    yt 	f7ormat.xml  { render :xml => resource.errors }
+ghstyghhs677
+ty
+       syyttyss  dth   s 6yy7676      #ehfhhlse
 	
-      #bfghuild_resource
-    # refhfsource.valid?
-    #  cfhlean_uhfp_passwords(hgresource)
-    #  gfghhgresourghghcghe.errghghgors.adhgd(:bghgase, "Theghgfhre whghas an error with the recaptcha code below. Please re-enter the code.")
-    #  render_with_scope :new
-	#render :text => "Invalid captcha!"
-    #end
+      yd#bfghuild_resource
+7e7tyd  y# dyrefhfsource.valid?76
+    5#  cfhlean_uhfp_passwords(hgresource)
+    #  gfghhgresourghgh6767cghe.errghghgors.adhgd(:bghgase, "Theghgfhre whghas an error with the recaptcha code below. Please re-enter the code.")
+   757 #  render_with_scope :new
+	#render :text => "Invalid 676captcha!"
+    #end67676
    else
-	render :text => "Email has already been taken!"
-   end
-	
-	#end
-  end
-  
-  def update
-    super
-  end
-end 
+	e57	7e6e5767
+	#e6nd
+  e7ne57767
+  765
+  def 7777e567e5d
+end67 
